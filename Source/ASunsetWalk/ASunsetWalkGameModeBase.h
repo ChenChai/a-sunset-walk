bb5e39ae351d26abe5b5bd11ac0289e87f0e133d
Chen Chai <chen.chai@outlook.com>
2020-04-15 21:10:17 -0400

    Initial commit

---
+++ Source/ASunsetWalk/ASunsetWalkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ASunsetWalkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASUNSETWALK_API AASunsetWalkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
